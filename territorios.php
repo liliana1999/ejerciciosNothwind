ad853371978d08749e69e2d34a454ae0efddf4ca
liliana1999 <48463561+liliana1999@users.noreply.github.com>
2019-05-09 11:24:33 -0500

    Update territorios.php

--- territorios.php
+++ territorios.php
@@ -1,1 +1,27 @@
+<?php
 
+//Require manda un error fatal y detiene la ejecucion
+//del programa si no se encuentra el archivo conexion.php
+
+require "conexion.php";
+
+//hace un llamado a una consulta
+
+$consulta = mysqli_query($conn,"Select * from Territorios");
+
+//mysqli_num_rows obtiene el numero de registros 
+
+$filas = mysqli_num_rows($consulta);
+echo "<h1 align= 'center'> Tabla Empleados </h1>";
+echo "<h1> cantidad de registro : " .$filas . " <br></h1>";
+echo "<Table border='1'>";
+echo "<tr>";
+echo "<th>IdTerritorio</th><th>DescripcionTerritorio</th><th>IdRegion</th>";
+echo "</tr>";
+while ($row = mysqli_fetch_array($consulta)) {
+    echo "<tr>";
+    echo "<td>".$row[0] ."</td>"."<td>".$row[1] ."</td>"."<td>".$row[2] ."</td>";
+    echo "</tr>";
+}
+echo "</table>"
+?>

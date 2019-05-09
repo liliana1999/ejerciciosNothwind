80a04cebff3380c5c4e1b103ac33e14cdaca87c1
liliana1999 <48463561+liliana1999@users.noreply.github.com>
2019-05-09 11:20:13 -0500

    Update detalles_pedido.php

--- detalles_pedido.php
+++ detalles_pedido.php
@@ -1,1 +1,26 @@
+<?php
 
+//require manda un error
+//si no encuentra el archivo 
+require "conexion.php";
+//hace un llamado a la consulta 
+$consulta = mysqli_query($conn,"select * from detalles_pedido");
+
+//mysqli_num_rows obtiene el numero de registros 
+$row= mysqli_num_rows($consulta);
+echo "<h1 align='center'> Consulta de Tabla detalles_pedido </h1> ";
+echo "<table border='1' align='center'>";
+echo "<tr>";
+echo "<th>Iddetallespedido</th><th>Idpedido</th><th>Idproducto</th>
+<th>PrecioUnitario</th><th>Cantidad</th><th>Descuento</th>";
+echo "</tr>";
+while($row = mysqli_fetch_array($consulta)){
+    echo "<tr>";
+    echo "<td>".$row[0]."</td>"."<td>".$row[1]."</td>".
+    "<td>".$row[2]."</td>"."<td>".$row[3]."</td>";
+    "<td>".$row[4]."</td>"."<td>".$row[5]."</td>";
+ 
+    echo "</tr>";
+}
+echo "<table>"
+?>

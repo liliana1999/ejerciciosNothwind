9f72ad3aed1335f455ba6426a06fa10c59ae5081
liliana1999 <48463561+liliana1999@users.noreply.github.com>
2019-05-09 11:21:17 -0500

    Update empleado_territorio.php

--- empleado_territorio.php
+++ empleado_territorio.php
@@ -1,1 +1,23 @@
+<?php
 
+//require manda un error
+//si no encuentra el archivo 
+require "conexion.php";
+//hace un llamado a la consulta 
+$consulta = mysqli_query($conn,"select * from pedidos");
+
+//mysqli_num_rows obtiene el numero de registros 
+$row= mysqli_num_rows($consulta);
+echo "<h1 align='center'> Consulta de Tabla pedidos </h1> ";
+echo "<table border='1' align='center'>";
+echo "<tr>";
+echo "<th>Idempleado</th><th>Idterritorio</th>";
+echo "</tr>";
+while($row = mysqli_fetch_array($consulta)){
+    echo "<tr>";
+    echo "<td>".$row[0]."</td>"."<td>".$row[1]."</td>".
+    
+    echo "</tr>";
+}
+echo "<table>"
+?>

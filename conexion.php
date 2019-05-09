6a2f55f634eed5f7c1bf7811059780eeda17869b
liliana1999 <48463561+liliana1999@users.noreply.github.com>
2019-05-09 11:19:36 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,17 @@
+<?php
 
+$dbhost = "127.0.0.1:3308";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "northwind";
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+mysqli_set_charset($conn,'utf-8');
+    //Chequea la conexion si es que da error
+    //Mysqli_connect_error => Devuelve una cadena con la descripcion 
+    //del ultimo error de conexion
+    If (!$conn) {
+        die("Connection failed: " . Mysqli_connect_error());
+    }
+
+
+?>
